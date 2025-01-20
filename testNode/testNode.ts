e590399265008f13fefc70698b57d43d87e7302f
Paul <mierlo.paul@hotmail.nl>
2025-01-21 00:41:14 +0800

    automatic cleanup

--- testNode/testNode.ts
+++ testNode/testNode.ts
@@ -1,13 +1,10 @@
 import { launchTestNode } from "@fuel-ts/contract/test-utils"
 
 const run = async () => {
-  const launchedWithDefaultConfig = await launchTestNode()
+  using launchedWithDefaultConfig = await launchTestNode()
   const { provider } = launchedWithDefaultConfig
   const { name } = await provider.fetchChain()
   console.log("Chain Name:", name)
-
-  // Ensure cleanup
-  launchedWithDefaultConfig.cleanup()
 }
 
 run()
