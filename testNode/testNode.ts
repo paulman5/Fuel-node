20f4ff3a143a2499d6f22ddcffb7e6dd4ba65d87
Paul <mierlo.paul@hotmail.nl>
2025-01-21 00:55:55 +0800

    no using statement

--- testNode/testNode.ts
+++ testNode/testNode.ts
@@ -1,7 +1,7 @@
 import { launchTestNode } from "@fuel-ts/contract/test-utils"
 
 const run = async () => {
-  using launchedWithDefaultConfig = await launchTestNode()
+  const launchedWithDefaultConfig = await launchTestNode()
   const { provider } = launchedWithDefaultConfig
   const { name } = await provider.fetchChain()
   console.log("Chain Name:", name)
