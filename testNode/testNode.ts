f598e5e6ad988a54cd7fa3fbc0d4442b2d74208c
Paul <mierlo.paul@hotmail.nl>
2025-01-21 14:40:52 +0800

    added endpoint

--- testNode/testNode.ts
+++ testNode/testNode.ts
@@ -1,10 +1,11 @@
 import { launchTestNode } from "@fuel-ts/contract/test-utils"
 
 const run = async () => {
-  using launchedWithDefaultConfig = await launchTestNode()
+  const launchedWithDefaultConfig = await launchTestNode()
   const { provider } = launchedWithDefaultConfig
   const { name } = await provider.fetchChain()
   console.log("Chain Name:", name)
+  console.log("Test node running at:", launchedWithDefaultConfig.provider.url)
 }
 
 run()
